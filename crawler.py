5fedab4fcf6e49234184b0ee3b246f9fc19d9e48
KIM-DO-WON <55446103+KIM-DO-WON@users.noreply.github.com>
2020-03-22 12:12:24 +0900

    Make the crawler python file. it is used in to scraping website

---
+++ crawler.py
@@ -0,0 +1,78 @@
+import requests
+from bs4 import BeautifulSoup
+
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+class Content:
+    def __init__(self, url, title, body):
+        self.url = url
+        self.title = title
+        self.body = body
+
+    def prints(self):
+        print("URL : {}".format(self.url))
+        print("TITLE : {}".format(self.title))
+        print("BODY :\n{}".format(self.body))
+
+class Website:
+    def __init__(self, name, url, titleTag, bodyTag):
+        self.name = name
+        self.url = url
+        self.titleTag = titleTag
+        self.bodyTag = bodyTag
+
+class Clawler:
+
+    def getPage(self, url):
+        try:
+            req = requests.get(url)
+        except requests.exceptions.RequestException:
+            return None
+        return BeautifulSoup(req.text, 'html.parser')
+
+    def safeGet(self, pageObj, selector):
+
+        #BeautifulSoup 객체와 선택자를 받아 콘텐츠 문자열을 추출하는 함수 주어진 선택자로 검색된
+        #결과가 없다면 빈 문자열을 반환합니다.
+
+        selectedElems = pageObj.select(selector)
+        if selectedElems is not None and len(selectedElems) > 0:
+            return '\n'.join([elem.get_text() for elem in selectedElems])
+        return ''
+
+    def parse(self, site, url):
+        #url을 받아 콘턴츠를 추출합니다.
+        bs = self.getPage(url)
+
+        if bs is not None:
+            title = self.safeGet(bs, site.titleTag)
+            body = self.safeGet(bs, site.bodyTag)
+            if title != '' and body != '':
+                content = Content(url, title, body)
+                content.prints()
+
+crawler = Clawler()
+
+siteData = [
+    ['O\'Reilly Media', 'http://oreilly.com', 'h1', 'section#product-description'],
+    ['Reuters', 'http://reuters.com', 'h1', 'div.StandardArticleBody_body_1gnLA'],
+    ['Brookings', 'http://brookings.edu', 'h1', 'div.post-body']
+]
+
+websites = []
+urls =[
+    'http://shop.oreilly.com/product/063920028154.do',
+    'http://www.reuters.com/article/us-usa-epa-pruitt-idUSKBN19W2D0',
+    'http://www.brookings.edu/blog/techtank/2016/03/01//idea-to-retire-old-methods-of-policy-education/'
+]
+
+for row in siteData:
+    websites.append(Website(row[0], row[1], row[2], row[3]))
+
+crawler.parse(websites[0], urls[0])
+crawler.parse(websites[1], urls[1])
+crawler.parse(websites[2], urls[2])
