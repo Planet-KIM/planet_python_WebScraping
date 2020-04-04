9eecf0597422c365298507b30dbc8418fe648fcc
KIM-DO-WON <55446103+KIM-DO-WON@users.noreply.github.com>
2020-04-04 12:46:39 +0900

    Used in is_article and fixed Rule class.

--- CrawlSpider/CrawlSpider/articles.py
+++ CrawlSpider/CrawlSpider/articles.py
@@ -6,30 +6,47 @@ class ArticleSpider(CrawlSpider):
     allowed_domains = ['wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/'
     'Benevolent_dictator_for_life']
-    rules = [Rule(LinkExtractor(allow=r'.*'), callback='parse_items', follow=True)]
 
-    def parse_items(self, response):
-        url = response.url
+    #rules = [Rule(LinkExtractor(allow=r'.*'), callback='parse_items', follow=True)]
+    rules = [
+        Rule(LinkExtractor(allow='en,wikipedia.org/wiki/((?!:).)*$'),
+            callback='parse_items', follow=True,
+            cb_kwargs={'is_article':True}),
+        Rule(LinkExtractor(allow='.*'), callback='parse_items',
+            cb_kwargs={'is_article':False})
+        ]
+
+    def parse_items(self, response, is_article):
+        print(response.url)
         title = response.css('h1::text').extract_first()
-        text = response.xpath('//div[@id="mw-content-text"]//text()').extract()
-        lastUpdated = response.css('li#footer-info-lastmod::text').extract_first()
-        lastUpdated = lastUpdated.replace('This page was last edited on ','')
+        if is_article:
+            url = response.url
+            text = response.xpath('//div[@id="mw-content-text"]''//text()').extract()
+            lastUpdated = response.css('li#footer-info-lastmod''::text').extract_first()
+            lastUpdated = lastUpdated.replace('This page was ''last edited on ','')
 
-        print('URL is {}'.format(url))
-        print('Title is {}'.format(title))
-        print('Text is {}'.format(text))
-        print('Last updated : {}'.format(lastUpdated))
+            print('URL is: {}'.format(title))
+            print('Last updated: {}'.format(lastUpdated))
+            print('Text is: {}'.format(text))
+        else:
+            print('This is not an article : {}'.format(title))
 
-    def parse_items(self, response):
-        url = response.url
+    def parse_items(self, response, is_article):
+        print(response.url)
         title = response.css('h1::text').extract_first()
 
-        text_rules = '//div[@id ="mw-content-text"]//text()'
-        text = response.xpath(text_rules).extract()
+        if is_article:
+            url = response.url
+            text_rules = '//div[@id ="mw-content-text"]//text()'
+            text = response.xpath(text_rules).extract()
 
-        update_rule = 'li#footer-info-lastmod::text'
-        update_delete = 'This page was last edited on '
-        lastUpdated = response.css(update_rule).extract_first()
-        lastUpdated = lastUpdated.replace(update_delete, '')
+            update_rule = 'li#footer-info-lastmod::text'
+            update_delete = 'This page was last edited on '
+            lastUpdated = response.css(update_rule).extract_first()
+            lastUpdated = lastUpdated.replace(update_delete, '')
 
-        print('URL is : {}'.format(url))
+            print('TITLE is : {}'.format(title))
+            print('last updated : {}'.format(lastUpdated))
+            print('Text is : {}'.format(text))
+        else:
+            print("This is not an article {}".format(title))
