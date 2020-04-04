d479117a3c1ab81524d43841bd32baca887a3d6a
KIM-DO-WON <55446103+KIM-DO-WON@users.noreply.github.com>
2020-04-04 14:29:23 +0900

    We used in pipeline

--- wikiSpider/wikiSpider/article.py
+++ wikiSpider/wikiSpider/article.py
@@ -3,7 +3,7 @@ from scrapy.spiders import CrawlSpider, Rule
 from wikiSpider.items import Article
 
 class ArticleSpider(CrawlSpider):
-    name  = 'articleItems'
+    name  = 'articlePipelines'
     allowed_domains = ['wikipedia.org']
     start_urls = ['https://wikipedia.org/wiki/Benevolent''_dictator_for_life']
     rules = [
@@ -16,8 +16,8 @@ class ArticleSpider(CrawlSpider):
         article['url'] = response.url
         article['title'] = response.css('h1::text').extract_first()
         article['text'] = response.xpath('//div[@id=''"mw-content-text"]//text()').extract()
-        lastUpdated = response.css('li#footer-info-lastmod::text').extract_first()
-        article['lastUpdated'] = lastUpdated.replace('This page was ''last edited on', '')
+        article['lastUpdated'] = response.css('li#''footer-info-lastmod::text').extract_first()
+        #article['lastUpdated'] = lastUpdated.replace('This page was ''last edited on', '')
         return article
 
     '''
--- wikiSpider/wikiSpider/pipelines.py
+++ wikiSpider/wikiSpider/pipelines.py
@@ -5,7 +5,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from datetime import datetime
+from wikiSpider.items import Article
+from string import whitespace
 
 class WikispiderPipeline(object):
-    def process_item(self, item, spider):
-        return item
+    def process_item(self, article, spider):
+        dateStr = article['lastUpdated']
+        dateStr = dateStr.replace('This page was last edited on', '')
+        dateStr = dateStr.strip()
+        dateStr = datetime.strptime(dateStr, '%d %B %Y, at %H:%M')
+        dateStr = dateStr.strftime('%Y-%m-%d %H:%M:%S')
+        article['lastUpdated'] = dateStr
+        
+        texts = article['text'][0:50]
+        texts = [line for line in texts if line not in whitespace]
+        article['text'] = ''.join(texts)
+        return article
+        
